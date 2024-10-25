c76ddd7921bd4c7f98f4f9a50cd9b08d3a8546ee
Li Xin Rui <li.xinrui3@gmail.com>
2024-10-25 14:03:00 -0400

    added aabb struct

---
+++ src/aabb.rs
@@ -0,0 +1,74 @@
+use crate::{
+    interval::Interval,
+    ray::{Point3, Ray},
+};
+
+#[derive(Default)]
+pub struct AABB {
+    x: Interval,
+    y: Interval,
+    z: Interval,
+}
+
+impl AABB {
+    pub fn new(x: Interval, y: Interval, z: Interval) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn with_points(a: &Point3, b: &Point3) -> Self {
+        let x = if a[0] <= b[0] {
+            Interval::new(a[0], b[0])
+        } else {
+            Interval::new(b[0], a[0])
+        };
+        let y = if a[1] <= b[1] {
+            Interval::new(a[1], b[1])
+        } else {
+            Interval::new(b[1], a[1])
+        };
+        let z = if a[2] <= b[2] {
+            Interval::new(a[2], b[2])
+        } else {
+            Interval::new(b[2], a[2])
+        };
+        Self { x, y, z }
+    }
+
+    pub fn axis_interval(&self, n: i32) -> &Interval {
+        match n {
+            1 => &self.y,
+            2 => &self.z,
+            _ => &self.x,
+        }
+    }
+
+    pub fn hit(&self, r: &Ray, ray_t: &mut Interval) -> bool {
+        let ray_orig = r.origin();
+        let ray_dir = r.direction();
+        for axis in 0..3 {
+            let ax = self.axis_interval(axis);
+            let adinv = 1. / ray_dir[axis as usize];
+            let t0 = (ax.min - ray_orig[axis as usize]) * adinv;
+            let t1 = (ax.max - ray_orig[axis as usize]) * adinv;
+            if t0 < t1 {
+                if t0 > ray_t.min {
+                    ray_t.min = t0
+                }
+                if t1 < ray_t.max {
+                    ray_t.max = t1
+                }
+            } else {
+                if t1 > ray_t.min {
+                    ray_t.min = t1
+                }
+                if t0 < ray_t.max {
+                    ray_t.max = t0
+                }
+            }
+            if ray_t.max <= ray_t.min {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/interval.rs
+++ src/interval.rs
@@ -1,3 +1,4 @@
+#[derive(Default)]
 pub struct Interval {
     pub min: f64,
     pub max: f64,
@@ -24,6 +25,14 @@ impl Interval {
 
     pub fn surrounds(&self, x: f64) -> bool {
         return x > self.min && x < self.max;
+    }
+
+    pub fn expand(&self, delta: f64) -> Self {
+        let padding = delta / 2.;
+        Self {
+            min: self.min - padding,
+            max: self.max + padding,
+        }
     }
 
     pub fn clamp(&self, x: f64) -> f64 {
