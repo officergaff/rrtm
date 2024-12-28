4a8bfabb4ce7e4faa7c1514cd8c22b0cc623181f
Li Xin Rui <li.xinrui3@gmail.com>
2024-12-27 20:52:14 -0500

    change config to support top-level await

--- rrtm-web/vite.config.ts
+++ rrtm-web/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
